7d153cbfaae0e9c3394f479ff5ce5ad766ef3aa1
Aniket Ratan <ratan.aniket09@gmail.com>
2023-05-11 18:55:12 +0530

    HighLighting the genre

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,10 @@ function App() {
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
-          <GenreList onSelectGenre={(genre) => setSlectedGenre(genre)} />
+          <GenreList
+            selectedGenre={selectedGenre}
+            onSelectGenre={(genre) => setSlectedGenre(genre)}
+          />
         </GridItem>
       </Show>
       <GridItem area="main">
